75edd088b071a440769b5f4d209608a9e0c9e9ab
Abdul Sennain <asennaini@gmail.com>
2023-03-05 06:10:23 -0800

    Successfully controlled a6000 using Sony API

---
+++ get_rpm.py
@@ -0,0 +1,21 @@
+from libsonyapi.camera import Camera
+from libsonyapi.actions import Actions
+from time import sleep
+"""
+This is a project that uses Sony's Camera Remote API to control an a6000. This camera 
+takes photos of a DC motor over time to visually determine its RPM with respect to 
+its decaying driving voltage. 
+"""
+
+"Camera Remote API by Sony"
+
+camera = Camera()  # create camera instance
+camera_info = camera.info()  # get camera camera_info
+# print(camera_info)
+# print(camera.api_version)  # print api version of camera
+
+camera.do(Actions.actTakePicture)
+# print(Camera.info(camera))
+
+
+  
