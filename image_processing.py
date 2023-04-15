5c47cfe8b2d985cb89726ed6cbce65e1776bb612
Abdul Sennain <asennaini@gmail.com>
2023-04-15 16:20:55 -0700

    Changed code as functions only

--- image_processing.py
+++ image_processing.py
@@ -4,15 +4,71 @@ import numpy as np
 from mpl_toolkits.axes_grid1 import ImageGrid
 import pandas as pd 
 import math
+import os
+
+def shift_yvals(line_points, image_height):
+    """
+    OpenCV represents y-values as top being zero, need to change
+    to traditional indexing.
+    """
+    for i in range(len(line_points)):
+        if line_points[i] not in [line_points[0], line_points[2]]:
+            line_points[i] = image_height - line_points[i]
+  
 
 
-def main():
+def find_angle_from_lines(line1, line2, flash_hertz):
+        """
+        line1 and line2 are np.array([xc, yc, x2, y2])
+        """
+        # find difference between x and y for each line
+        line1 = np.array([line1[2]-line1[0], line1[3]-line1[1]]) 
+        line2 = np.array([line2[2]-line2[0], line2[3]-line2[1]]) 
+        final_angle = math.acos(   np.dot(line1, line2)    /    ((np.linalg.norm(line1))*(np.linalg.norm(line2))) ) * 180/np.pi
+        final_rpm = final_angle * flash_hertz * 60/360
 
-    # Create list where all images are stored
-    image_list = []
+        return round(final_rpm, 2)
+
+def center_point_first(line1_points, line2_points):
+    # sourcery skip: merge-list-extend, unwrap-iterable-construction
+    """
+    This function ensures that the array used to reprsent each line starts
+    with the center point first, which is important for dot product. 
+    """
+    point_differences = []
+    point_differences.extend(
+        (
+            np.sum(np.abs(line1_points[:2] - line2_points[:2])),
+            np.sum(np.abs(line1_points[:2] - line2_points[2:])),
+            np.sum(np.abs(line1_points[2:] - line2_points[:2])),
+            np.sum(np.abs(line1_points[2:] - line2_points[2:])),
+        )
+    )
+    # find which condition in the list sets center points based on least value in point_differences[] 
+    # ... if difference between two points is very small, this is in indication those are our center points for both lines
+    point_differences.index(min(point_differences))
+
+
+    if point_differences.index(min(point_differences)) == 0:
+    # do nothing, points are already in correct np.array([xc, yc, x1, y1]) form
+        pass
+    elif point_differences.index(min(point_differences)) == 1:
+        # first line is in correct form, but second needs to switch first point
+        line2_points = np.append(line2_points[2:], line2_points[:2])
+
+    elif point_differences.index(min(point_differences)) == 2:
+        # second line is in correct form, but first line needs to switch first point
+        line1_points = np.append(line1_points[2:], line1_points[:2])
+
+    elif point_differences.index(min(point_differences)) == 3:
+        # Both lines need to be switched and redefined
+        line1_points = np.append(line1_points[2:], line1_points[:2])
+        line2_points = np.append(line2_points[2:], line2_points[:2])
+
+def get_image_data(folder, image, flash_hertz):
 
     # Loading image from file path
-    test_image = cv.imread(filename = r"D:\DCIM\16430414\DSC01959.JPG")
+    test_image = cv.imread(filename = os.path.join(folder, image))
 
     # Resize image to (800, 1200)
     # Note that camera has 24 MP resolution 
@@ -22,16 +78,11 @@ def main():
     dim = (width, height)
     test_image = cv.resize(test_image, dim)
 
-    # Store original resized image in list 
-    image_list.append(test_image)
-
     # Convert image to grayscale 
     test_image_gray = cv.cvtColor(test_image, cv.COLOR_BGR2GRAY)
 
-
     # Binarize image
     test_image_bin = cv.threshold(test_image_gray, 120, 255, cv.THRESH_BINARY)[1]
-
 
     #! Test to see which threshold value yields max number of lines detected, use that threshold 
     threshold_xvals = []
@@ -88,9 +139,11 @@ def main():
     line1_df = group_lines_df_sorted[:line2_index].reset_index().drop('index', axis = 1).drop('angle', axis = 1)
     line2_df = group_lines_df_sorted[line2_index:].reset_index().drop('index', axis = 1).drop('angle', axis = 1)
 
-    # Highlight line #1 using average value points from respective line 1 DataFrame
+    # Highlight lines using average value points from respective line 1 DataFrame
     cv.line(test_image, (int(line1_df.mean(axis = 0)[0]), int(line1_df.mean(axis = 0)[1])), (int(line1_df.mean(axis = 0)[2]), int(line1_df.mean(axis = 0)[3])), (0, 255, 0), 10)
     cv.line(test_image, (int(line2_df.mean(axis = 0)[0]), int(line2_df.mean(axis = 0)[1])), (int(line2_df.mean(axis = 0)[2]), int(line2_df.mean(axis = 0)[3])), (0, 255, 0), 10)
+    
+
 
     # Unformatted lines as list, not in np.array([xc,yc,x2,y2]) form, need center points to come first
     line1_points = line1_df.mean(axis = 0).astype(int).values.tolist()
@@ -109,6 +162,12 @@ def main():
     # Set point types based off of point_differences (identify center)
     # This is where first two points are already center points for both lines 
     center_point_first(line1_points, line2_points)
+    # Highlight center points for each line
+    cv.circle(test_image, tuple(line1_points[:2]), radius=20, color=(255, 0, 0), thickness=-2)
+    cv.circle(test_image, tuple(line2_points[:2]), radius=20, color=(255, 0, 0), thickness=-2)
+    # Highlight end points for each line 
+    cv.circle(test_image, tuple(line1_points[2:]), radius=20, color=(255, 255, 0), thickness=-2)
+    cv.circle(test_image, tuple(line2_points[2:]), radius=20, color=(255, 255, 0), thickness=-2)
 
     # Make sure points reflect normal x-y plane, openCV treats top of image as zero y point, set bottom as zero 
     shift_yvals(line1_points, image_height = height)
@@ -119,96 +178,6 @@ def main():
     # print(f"Number of lines detected: {len(lines_list)}")
 
 
-    # # Create a figure
-    fig = plt.figure(figsize=(6, 6), facecolor="Black")
-    grid = ImageGrid(
-                        fig, 
-                        111,  # similar to subplot(111)
-                        nrows_ncols=(1,4),  # creates 2x2 grid of axes
-                        axes_pad=0.1,  # pad between axes in inch.
-
-                    )
-
-    # Plot them bad boys (processed images)
-    for ax, image in zip(grid, image_list):
-        # Iterating over the grid returns the Axes.
-        ax.imshow(image, cmap = 'gray')
-
     # Find final angle
-    print(find_angle_from_lines(line1_points, line2_points))
-
-    # Show the finished plot 
-    plt.show()
-
-    
-   
-
-
-
-
-
-
-
-
-
-
-
-def shift_yvals(line_points, image_height):
-    """
-    OpenCV represents y-values as top being zero, need to change
-    to traditional indexing.
-    """
-    for i in range(len(line_points)):
-        if line_points[i] not in [line_points[0], line_points[2]]:
-            line_points[i] = image_height - line_points[i]
-  
-
-
-def find_angle_from_lines(line1, line2):
-        """
-        line1 and line2 are np.array([xc, yc, x2, y2])
-        """
-        # find difference between x and y for each line
-        line1 = np.array([line1[2]-line1[0], line1[3]-line1[1]]) 
-        line2 = np.array([line2[2]-line2[0], line2[3]-line2[1]]) 
-        final_angle = math.acos(   np.dot(line1, line2)    /    ((np.linalg.norm(line1))*(np.linalg.norm(line2))) ) * 180/np.pi
-
-        return round(final_angle, 2)
-
-def center_point_first(line1_points, line2_points):
-    """
-    This function ensures that the array used to reprsent each line starts
-    with the center point first, which is important for dot product. 
-    """
-    point_differences = []
-    point_differences.extend(
-        (
-            np.sum(np.abs(line1_points[:2] - line2_points[:2])),
-            np.sum(np.abs(line1_points[:2] - line2_points[2:])),
-            np.sum(np.abs(line1_points[2:] - line2_points[:2])),
-            np.sum(np.abs(line1_points[2:] - line2_points[2:])),
-        )
-    )
-    # find which condition in the list sets center points based on least value in point_differences[] 
-    # ... if difference between two points is very small, this is in indication those are our center points for both lines
-    point_differences.index(min(point_differences))
-
-
-    if point_differences.index(min(point_differences)) == 0:
-    # do nothing, points are already in correct np.array([xc, yc, x1, y1]) form
-        pass
-    elif point_differences.index(min(point_differences)) == 1:
-        # first line is in correct form, but second needs to switch first point
-        line2_points = np.append(line2_points[2:], line2_points[:2])
-
-    elif point_differences.index(min(point_differences)) == 2:
-        # second line is in correct form, but first line needs to switch first point
-        line1_points = np.append(line1_points[2:], line1_points[:2])
-
-    elif point_differences.index(min(point_differences)) == 3:
-        # Both lines need to be switched and redefined
-        line1_points = np.append(line1_points[2:], line1_points[:2])
-        line2_points = np.append(line2_points[2:], line2_points[:2])
-
-if __name__ == "__main__":
-    main()
+    angular_velocity = find_angle_from_lines(line1_points, line2_points, flash_hertz)
+    return test_image, line1_points, line2_points, angular_velocity
